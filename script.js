b35896bc2f35fac0bd9c0a2563db2ad2157bff62
Ar_Lazy_Dev <arghaghosh2801@gmail.com>
2023-02-21 15:37:22 +0530

    Refactored the code a bit and added monster list

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 //declaring variables
 
 let xp = 0;
-let health = 500;
+let health = 100;
+let maxHealth = 100;
 let currentWeapon = 0;
 let gold = 50;
 let monsterHealth;
@@ -47,7 +48,7 @@ const locations = [
         "button functions": [fightSlime,fightScribble,exitCave],
         text: "You are inside the store now, you see racks of potion, swords, daggers and many other weapons. What do you want to do?"
     }
-]
+];
 
 const weapons = [
     {
@@ -66,14 +67,31 @@ const weapons = [
         name: "Sword of Exibus",
         power: 150
     }
-]
+];
 
+const monsters = [
+    {
+        name: "Slime",
+        lvl: 2,
+        health: 15
+    },
+    {
+        name: "Scribble",
+        lvl: 8,
+        health: 60
+    },
+    {
+        name: "Frost Dragon",
+        lvl: 20,
+        health: 300
+    }
+];
 
 //intializing buttons
 
-button1.onclick = goTown;
-button2.onclick = goCave;
-button3.onclick = sleep;
+button1.onclick = goTown();
+button2.onclick = goCave();
+button3.onclick = sleep();
 
 
 function update(location){
@@ -110,12 +128,12 @@ function exitCave() {
 }
 
 function sleep() {
-    if(health <= 450){
-        health+=50;
+    if(health <= (maxHealth-20)){
+        health+=20;
         healthValue.innerText = health;
         text.innerText="You have well slept and recovered your heath.";
     }else{
-        health = 500;
+        health = maxHealth;
         healthValue.innerText = health;
         text.innerText="You have well slept and recovered your heath.";
     }
@@ -124,19 +142,24 @@ function sleep() {
 
 function buyHealth() {
     if(gold>=10){
-        if(health<=490){
+        if(health <= (maxHealth-10)){
             gold-=10;
             health+=10;
             goldValue.innerText = gold;
             healthValue.innerText = health;
             text.innerText = "you successfully bought 10 health :)";
-        }else{
-            let amount = 500-health;
+        }else if(health<maxHealth){
+            let amount = maxHealth-health;
             gold -=amount;
             goldValue.innerText = gold;
-            health = 500;
+            health = maxHealth;
             healthValue.innerText = health;
-            text.innerText = "you successfully bought " + amount + "health :)";
+            text.innerText = "you successfully bought " + amount + " health :)";
+        }else{
+            maxHealth+=10;
+            health = maxHealth;
+            healthValue.innerText = health;
+            text.innerText = "your increased maximum health is now: " + maxHealth;
         }
     }else{
         text.innerText = "You don't have enough gold to buy health. Go earn some coins and visit again :)";
@@ -144,27 +167,54 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-    if(gold>=30){
-        gold-=30;
-        goldValue.innerText = gold;
-        currentWeapon++;
-        let newWeapon = weapons[currentWeapon].name;
-        text.innerText = "You now have a "+ newWeapon + ".";
-        inventory.push(newWeapon);
-        text.innerText += "In your inventory now you have " + inventory;
+    if(currentWeapon < weapons.length - 1){
+        if(gold>=30){
+            gold-=30;
+            goldValue.innerText = gold;
+            currentWeapon++;
+            let newWeapon = weapons[currentWeapon].name;
+            text.innerText = "You now have a "+ newWeapon + ".";
+            inventory.push(newWeapon);
+            text.innerText += "In your inventory now you have " + inventory;
+        }else{
+            text.innerText = "You don't have enough gold to buy any weapon. Go earn some coins and visit again :)";
+        }
     }else{
-        text.innerText = "You don't have enough gold to buy any weapon. Go earn some coins and visit again :)";
+        text.innerText = "You already have the most powerful weapon :)";
+        button2.innerText = "Sell Weapon";
+        button2.onclick = sellWeapon;
     }
 }
 
-function fightSlime() {
+function sellWeapon(){
+    if(inventory.length>1){
+        gold+=20;
+        goldValue.innerText = gold;
+        let currentSellingWeapon = inventory.shift;
+        text.innerText = "You sold a " + currentSellingWeapon + " for 20 gold.";
+        text.innerText += "In your inventory you now have:"+ inventory;
+    }else{
+        text.innerText = "You can't sell your only weapon."
+    }
+}
 
+
+
+function fightSlime() {
+    fighting = 0;
+    goFight();
 }
 
 function fightScribble() {
-    
+    fighting = 1;
+    goFight();
 }
 
 function fightDragon() {
+    fighting = 2;
+    goFight();
+}
 
+function goFight() {
+    
 }
